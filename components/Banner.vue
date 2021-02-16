b0cfa7ee1fbc828cff536389b1a086565a8b1ce9
BisiDavi <oludavidconnect@gmail.com>
2021-02-16 01:56:43 +0100

    added some components and moved images

---
+++ components/Banner.vue
@@ -0,0 +1,9 @@
+<template>
+  <b-container class="banner">
+    <b-row>
+      <b-col>
+          <p>I am the banner </p>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
---
+++ components/Thumbnailcard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-card img-src="require(`~assets/images/${thumbnailImg}.png`)" alt="thumbnailAlt"> </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Thumbnailcard",
+  props: {
+    thumbnailImg: {
+      type: String,
+      required: true
+    },
+    thumbnailAlt: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
